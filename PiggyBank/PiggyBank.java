50d56f639a4788720fe536c79e915201425da58b
BrandonComins <brandcomin@gmail.com>
2022-04-13 10:37:52 -0700

    add function to check valid Coins

--- PiggyBank/PiggyBank.java
+++ PiggyBank/PiggyBank.java
@@ -5,15 +5,12 @@ public class PiggyBank {
 
     //Constructor: this is called when you create an object
     public PiggyBank(int q, int d, int n, int p){
-        assert q >= 0 : "can't have negative quarters";
-        assert d >= 0 : "can't have negative dimes";
-        assert n >= 0 : "can't have negative nickels";
-        assert p >= 0 : "can't have negative pennies";
-        
         this.quarters = q;
         this.dimes = d;
         this.nickels = n;
         this.pennies = p;
+
+        checkValidCoins(this.quarters, this.dimes, this.nickels, this.pennies);
     }
 
     //Overloading Constuctor, this will be called instead if you put no parameters
@@ -44,5 +41,26 @@ public class PiggyBank {
         System.out.println("dimes: "    + this.dimes);
         System.out.println("nickels: "  + this.nickels);
         System.out.println("pennies: "  + this.pennies);
+    }
+
+    public void checkValidCoins(int q, int d, int n, int p){
+        if(q < 0){
+            System.out.printl("Can't put negative quarters in, Q = 0");
+            this.quarters = 0;
+        }
+        if (d < 0){
+            System.out.println("Can't put negative dimes in, D = 0");
+            this.dimes = 0;
+        }
+        if (n < 0){
+            System.out.println("Can't put negative nickels in, N = 0");
+            this.nickels = 0;
+        }
+        
+        if (p < 0){
+            System.out.println("Can't have negative pennies, P = 0");
+            this.pennies = 0;
+        }
+
     }
 }
