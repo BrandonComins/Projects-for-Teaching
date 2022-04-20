708a6ca5ad938e633d6b0e02a989f1b11161d8f2
BrandonComins <brandcomin@gmail.com>
2022-04-20 14:46:40 -0700

    add function to remove change, and add more error handling

--- PiggyBank/src/PiggyBank.java
+++ PiggyBank/src/PiggyBank.java
@@ -1,7 +1,7 @@
 package PiggyBank;
 
 public class PiggyBank {
-    int quarters, dimes, nickels, pennies; 
+    private int quarters, dimes, nickels, pennies; 
 
     //Constructor: this is called when you create an object
     public PiggyBank(int q, int d, int n, int p){
@@ -38,18 +38,62 @@ public class PiggyBank {
         right.pennies = 0;
     }
 
+    /*Bonus points if you can do this function recursively, 
+    but I haven't taught it at this point */
+    public void removeExactMoney(double money){ 
+        int num_quarters = 0, num_dimes = 0, num_nickels = 0, num_pennies = 0;
+        double initial_money = money;
+        
+
+        //Ask me why or look up I need round for comparing doubles
+        while(Math.round(money * 100.0) / 100.0 != 0.00){
+            if(this.quarters > 0 && money >= .25){
+                this.quarters -= 1;
+                money -= .25;
+                ++num_quarters;
+                continue;
+            }
+            else if(this.dimes > 0 && money >= .1){
+                this.dimes -= 1;
+                money -= .1;
+                ++num_dimes;
+                continue;
+            }
+            else if (this.nickels > 0 && money >= .05){
+                this.nickels -= 1;
+                money -= .05;
+                ++num_nickels;
+                continue;
+            }
+            else if (this.pennies > 0 && money >= .01){
+                this.pennies -= 1;
+                money -= .01;
+                ++num_pennies;
+                continue;
+            }else{
+                insertChange(num_quarters, num_dimes, num_nickels, num_pennies);
+                System.out.printf("Not enough money in bank to pay: $%.2f \n",
+                                    initial_money);
+                System.out.print("Current change: ");
+                printTotalAmount();
+                break;
+            }
+        }
+    }
+
     public void printTotalAmount(){
         double total = this.quarters * .25 + this.dimes * .1
             + this.nickels * .05 + this.pennies * .01;
 
         System.out.printf("$%.2f", total);
+        System.out.println('\n');
     }
 
     public void printCoins(){
-        System.out.println("quarters: " + this.quarters);
-        System.out.println("dimes: "    + this.dimes);
-        System.out.println("nickels: "  + this.nickels);
-        System.out.println("pennies: "  + this.pennies);
+        System.out.println("Quarters: " + this.quarters);
+        System.out.println("Dimes: "    + this.dimes);
+        System.out.println("Nickels: "  + this.nickels);
+        System.out.println("Pennies: "  + this.pennies);
     }
 
     private int checkValidQuarters(int q){
